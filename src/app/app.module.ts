1790ec0c46c17927b22503a8777dab37ba149ce9
Davi Cancelier <davi.cancelier@gmail.com>
2019-04-24 19:36:12 -0300

    atualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import {TableModule} from 'primeng/table';
-import {TooltipModule, Tooltip} from 'primeng/tooltip';
+import {TooltipModule} from 'primeng/tooltip';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
